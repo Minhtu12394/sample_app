da49e03e4d00243a0bc5b7ff6a52dbcacdeb425d
Minh Tu <minhtu12394@gmail.com>
2017-09-25 18:21:26 +0700

    chuong 6

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
   root "static_pages#home"
   get  "/help"    => "static_pages#help", as: "helf"
   get  "/about"   => "static_pages#about"
   get  "/contact" => "static_pages#contact"
+  resources :users
  end
